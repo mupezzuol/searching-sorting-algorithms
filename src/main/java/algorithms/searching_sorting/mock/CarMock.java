8c957e80f419a39495e90480c0fe621ec047f518
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-07-03 21:49:33 +0100

    Add test select and insertion sort

--- src/main/java/algorithms/searching_sorting/mock/CarMock.java
+++ src/main/java/algorithms/searching_sorting/mock/CarMock.java
@@ -7,7 +7,7 @@ public class CarMock {
 	public Car[] getMockCars() {
 		Car[] cars = {
 				new Car("Lamborghine", 1000000d),
-				new Car("Jipe", 46000d),
+				new Car("Jipe", 46001d),
 				new Car("Brasília", 16000d),
 				new Car("Smart", 46000d),
 				new Car("Fusca", 17000d),
@@ -15,6 +15,19 @@ public class CarMock {
 				new Car("Corsa", 15000d),
 		};
 		return cars;
+	}
+	
+	public Car[] getMockCarsSorted() {
+		Car[] carsOrder = {
+				new Car("Corsa", 15000d),
+				new Car("Brasília", 16000d),
+				new Car("Fusca", 17000d),
+				new Car("Smart", 46000d),
+				new Car("Jipe", 46001d),
+				new Car("Lamborghine", 1000000d),
+				new Car("Ferrari", 1230000d),
+		};
+		return carsOrder;
 	}
 
 }
