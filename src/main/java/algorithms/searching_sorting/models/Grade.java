cc484c03c42abd585715a8b54566bcaa8932eea4
Murillo Pezzuol <murillopezzuol@hotmail.com>
2020-07-13 23:18:45 +0100

    Add Merge two arrays

---
+++ src/main/java/algorithms/searching_sorting/models/Grade.java
@@ -0,0 +1,21 @@
+package algorithms.searching_sorting.models;
+
+public class Grade {
+
+    private String student;
+    private double value;
+
+    public Grade(String student, double value) {
+        this.student = student;
+        this.value = value;
+    }
+
+    public String getStudent() {
+        return student;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+}
